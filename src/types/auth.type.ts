8b053039f3060ef57472b1600e4aa0c33f704135
Nipuana <nipuanarimal@gmail.com>
2026-01-05 20:02:48 +0545

    edited auth type

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,11 +1,9 @@
 import z from 'zod';
 export const userType=z.object({
-    firstName:z.string().optional(),
-    lastName:z.string().optional(),
     email:z.email(),
     username:z.string().min(3),
     password:z.string().min(6),
-    role : z.enum(['user','admin']).default('user')
+    role : z.enum(['user','artist','admin']).default('user')
 });
 
 export type UserType=z.infer<typeof userType>;
