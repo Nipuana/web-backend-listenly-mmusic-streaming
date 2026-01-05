691ee3a35153d96ba7932190bc6f2ec7eae40c75
Nipuana <nipuanarimal@gmail.com>
2026-01-05 19:59:21 +0545

    made auth type

---
+++ src/model/auth.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema  } from "mongoose"
+import { UserType } from "../types/auth.type"
+
+const UserSchema: Schema = new Schema(
+    {
+        firstName: { type: String},
+        lastName: { type: String},
+        email: { type: String, required: true, unique: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ['user','admin'], default: 'user' }
+    },
+    {
+        timestamps: true,
+    }
+)
+export interface IUser extends UserType, Document {
+    _id: mongoose.Types.ObjectId; // mongo related attribute
+    createdAt: Date;
+    updatedAt: Date; 
+}
+
+export const UserModel= mongoose.model<IUser>("User", UserSchema);
+// collection name "users" ("plural of User")
+// UserModel -> db.users
---
+++ src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import {  AuthController } from "../controllers/auth.controller.ts";
+const router=Router();
+const authController=new AuthController();
+
+router.post('/register', authController.registerUser);
+router.post('/login', authController.loginUser);
+
+export default router;
