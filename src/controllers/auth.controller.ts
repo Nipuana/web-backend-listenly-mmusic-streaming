3f44a6d4323d0d9c51eb7043f4915c2b07d68a17
Nipuana <nipuanarimal@gmail.com>
2026-01-23 11:33:11 +0545

    made logicfor update user in controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,12 @@
 import { AuthService } from "../services/auth.service";
-import { CreateUserDto, LoginUserDto } from "../dtos/auth.dtos";
+import { CreateUserDto, LoginUserDto, UpdateUserDto } from "../dtos/auth.dtos";
+import { UserRepository } from "../repositories/auth.repository";
 import  z, { success } from "zod";
 import { Request, Response } from "express";
+import { HttpError } from "../errors/http-error";
+import bcrypt from "bcryptjs/umd/types";
 let authService = new AuthService();
-
+let userRepository = new UserRepository();
 export class AuthController{
 
     async registerUser(req: Request, res: Response){
@@ -64,5 +67,30 @@ async getUserById(req:Request, res:Response){
             return res.status(error.statusCode).json(
                 {success:false, message: error.message || "Internal Server Error"});
         }
+    }
+
+async updateUser(userId: string, data: UpdateUserDto){
+        const user = await userRepository.getUserById(userId);
+        if(!user){
+            throw new HttpError(404, "User not found");
+        }
+        if(user.email !== data.email){
+            const emailExists = await userRepository.getUserByEmail(data.email!);
+            if(emailExists){
+                throw new HttpError(409, "Email already exists");
+            }
+        }
+        if(user.username !== data.username){
+            const usernameExists = await userRepository.getUserByUsername(data.username!);
+            if(usernameExists){
+                throw new HttpError(409, "Username already exists");
+            }
+        }
+        if(data.password){
+            const hashedPassword = await bcrypt.hash(data.password, 10);
+            data.password = hashedPassword;
+        }
+        const updatedUser = await userRepository.updateUserById(userId, data);
+        return updatedUser;
     }
 }
--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -3,7 +3,8 @@ export const userType=z.object({
     email:z.email(),
     username:z.string().min(3),
     password:z.string().min(6),
-    role : z.enum(['user','artist','admin']).default('user')
+    role : z.enum(['user','artist','admin']).default('user'),
+    profilePicture: z.string().optional()
 });
 
 export type UserType=z.infer<typeof userType>;
