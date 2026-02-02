5c239a08b3eb1b34ce1e931474739f7d74c2a286
Nipuana <nipuanarimal@gmail.com>
2026-02-02 18:52:29 +0545

    updated routes calling

--- src/routes/userInfo.route.ts
+++ src/routes/userInfo.route.ts
@@ -5,8 +5,8 @@ import { authorizedMiddleware } from "../middlewares/authorization.middleware";
 const router = Router();
 const userInfoController = new UserInfoController();
 
-router.put('/additional-info', authorizedMiddleware, userInfoController.updateAdditionalInfo);
-router.get('/additional-info', authorizedMiddleware, userInfoController.getAdditionalInfo);
-router.delete('/additional-info', authorizedMiddleware, userInfoController.clearAdditionalInfo);
+router.put('/add-info', authorizedMiddleware, userInfoController.updateAdditionalInfo);
+router.get('/get-info', authorizedMiddleware, userInfoController.getAdditionalInfo);
+router.delete('/clear-info', authorizedMiddleware, userInfoController.clearAdditionalInfo);
 
 export default router;
